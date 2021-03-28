1092c2717b917bbe67f6328cd769a5b67fe0e4cf
sinead4 <sinead.ganz@students.fhnw.ch>
2021-03-28 19:03:51 +0200

    TestClass ContactsPageIT erstellt

---
+++ contactlist-thymeleaf/src/test/java/webeng/contactlist/ContactsPageIT.java
@@ -0,0 +1,29 @@
+package webeng.contactlist;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.util.AssertionErrors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ContactsPageIT {
+
+    @LocalServerPort
+    int port;
+
+    @Test
+    void allContactListEntriesAreShown(){
+        var driver = new HtmlUnitDriver();
+        driver.navigate().to("https://localhost:"+port +"/contacts");
+
+        var contactLinks = driver.findElements(By.cssSelector("main ul a"));
+        assertEquals(30, contactLinks.size());
+
+
+    }
+
+}
